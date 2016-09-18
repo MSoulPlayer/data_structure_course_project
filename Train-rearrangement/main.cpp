040e658be037b7a1f728adc7188d6b417cb2be08
Msoul <maozihao1997@126.com>
2016-09-18 20:50:02 +0800

    Add files via upload

---
+++ Train-rearrangement/main.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<queue>
+#include<string>
+
+using namespace std;
+
+int char2int(char ch) {
+    return ch - '0';
+}
+
+
+int main(void) {
+    string str;
+    cin >> str;
+    queue<char> q;
+    vector<stack<char>> v;
+    for (int i = str.size() - 1; i >= 0; i--) {
+        if (str[i] == '1') {
+            q.push(str[i]);
+            cout << "The 1 get into queue" << endl;
+        } else {
+            if (v.empty()) {
+                stack<char> s;
+                s.push(str[i]);
+                v.push_back(s);
+                cout << "number " << str[i] << " get into stack 1" << endl;
+            } else {
+                bool flag = false;
+                int count = 0;
+                for (auto iter = v.begin(); iter != v.end(); iter++) {
+                    count++;
+                    if ((*iter).top() - 1 == str[i]) {
+                        (*iter).push(str[i]);
+                        cout << "number " << str[i] << " get into stack " << count << endl;
+                        flag = true;
+                    }
+                }
+                if (!flag) {
+                    stack<char> temp;
+                    temp.push(str[i]);
+                    v.push_back(temp);
+                    cout << "number " << str[i] << " get into stack " << v.size() << endl;
+                }
+            }
+        }
+    }
+    int count_ = 0;
+    for (auto iter = v.begin(); iter != v.end(); iter++) {
+        count_++;
+        if ((*iter).top() == q.back()+1) {
+            while(!(*iter).empty()) {
+                q.push((*iter).top());
+                cout << (*iter).top() << " in stack " << count_ << " get into queue" << endl; 
+                (*iter).pop();
+            }
+        }
+    }
+    return 0;
+}
