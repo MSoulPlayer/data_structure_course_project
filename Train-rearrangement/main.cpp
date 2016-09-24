8fcca812690bee4d4a0bebe3ca65af8284b6ae2a
Msoul <maozihao1997@126.com>
2016-09-24 11:54:42 +0800

    Update main.cpp

--- Train-rearrangement/main.cpp
+++ Train-rearrangement/main.cpp
@@ -6,16 +6,12 @@
 
 using namespace std;
 
-int char2int(char ch) {
-    return ch - '0';
-}
-
 
 int main(void) {
     string str;
     cin >> str;
     queue<char> q;
-    vector<stack<char>> v;
+    vector<stack<char> > v;
     for (int i = str.size() - 1; i >= 0; i--) {
         if (str[i] == '1') {
             q.push(str[i]);
@@ -29,7 +25,7 @@ int main(void) {
             } else {
                 bool flag = false;
                 int count = 0;
-                for (auto iter = v.begin(); iter != v.end(); iter++) {
+                for (vector<stack<char> >::iterator iter = v.begin(); iter != v.end(); iter++) {
                     count++;
                     if ((*iter).top() - 1 == str[i]) {
                         (*iter).push(str[i]);
@@ -46,10 +42,14 @@ int main(void) {
             }
         }
     }
-    int count_ = 0;
-    for (auto iter = v.begin(); iter != v.end(); iter++) {
+    
+    int temp = str.length() - 1;
+    while (temp--) {
+        int count_ = 0;
+        for (vector<stack<char> >::iterator iter = v.begin(); iter != v.end(); iter++) {
         count_++;
-        if ((*iter).top() == q.back()+1) {
+        if ((*iter).empty()) {
+        } else if ((*iter).top() == q.back()+1) {
             while(!(*iter).empty()) {
                 q.push((*iter).top());
                 cout << (*iter).top() << " in stack " << count_ << " get into queue" << endl; 
@@ -57,5 +57,6 @@ int main(void) {
             }
         }
     }
+}
     return 0;
 }
